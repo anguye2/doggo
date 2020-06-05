276ddea5bfba0b0cf1a1cfa90b78bf039a6f6a06
Long Nguyen <longnguyen@Longs-MacBook-Pro.local>
2020-06-05 05:15:56 -0700

    popup

--- static/script.js
+++ static/script.js
@@ -1,3 +1,6 @@
-$('#dog-dropdown')
-  .dropdown()
-;
+$("#dog-dropdown").dropdown();
+
+$(".love-btn").click(function () {
+  console.log("clicked love btn")
+  $(".ui.mini.modal").modal("show");
+});
